b7e15d49a1951914ff35e040285285f7b3422448
thumbd12856@gmail.com <thumbd12856@gmail.com>
2016-03-30 20:01:48 +0800

    boostrap/cache, storage/*: director

---
+++ storage/framework/views/450da0ae793144c14aeec34ef8ac541d6983c593.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+    <?php echo $__env->yieldContent('css'); ?>
+</head>
+<body>
+    <?php echo $__env->yieldContent('content'); ?>
+
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-1.12.2.min.js" integrity="sha256-lZFHibXzMHo3GGeehn1hudTAP3Sc0uKXBXAzHX1sjtk=" crossorigin="anonymous"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    <?php echo $__env->yieldContent('js'); ?>
+</body>
+</html>
