d1083a0d5b3a63ddacfb4309727dab7c34e31ef4
teddy8478 <teddy8478@gmail.com>
2016-05-11 23:09:50 +0800

    add company/group

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -39,6 +39,8 @@ Route::group(['prefix'=>'feeManager'], function() {
 
 Route::group(['prefix'=>'accountManager'], function() {
     Route::get('account', 'AccountManagerController@account');
+    Route::get('company', 'AccountManagerController@company'); 
+     
 });
 
 Route::group(['prefix'=>'master'], function() {
