8181e50b2db56a752a9032a4f1ab6c1821f232f7
FATESAIKOU <qzsecftbhhhh@gmail.com>
2016-05-17 16:50:35 +0800

    Add user_id to table : user_quotas

--- database/migrations/2016_05_16_181733_create_user_quotas_table.php
+++ database/migrations/2016_05_16_181733_create_user_quotas_table.php
@@ -14,11 +14,13 @@ class CreateUserQuotasTable extends Migration
     {
         Schema::create('user_quotas', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id')->unsigned();
             $table->integer('period_id')->unsigned();
             $table->integer('quota');
 
             $table->timestamps();
 
+            $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade');
             $table->foreign('period_id')->references('id')->on('periods')->onUpdate('cascade');
         });
     }
