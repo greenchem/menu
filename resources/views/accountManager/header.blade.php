4a1e55a28121ea90921b420ce8950393a8e63459
demonic <ian910297@gmail.com>
2016-06-05 14:46:09 +0800

    Remove Error Link

--- resources/views/accountManager/header.blade.php
+++ resources/views/accountManager/header.blade.php
@@ -1,8 +1,7 @@
 <ul class="nav nav-tabs">
-    <li role="presentation"><a href="{{url('accountManager/account')}}">帳號</a></li>
+    <li role="presentation"><a href="{{url('accountManager/account')}}">帳號管理</a></li>
 
     <li role="presentation"><a href="{{url('accountManager/company')}}">公司部門設定</a></li>
-    <li role="presentation"><a href="{{url('accountManager/fee')}}">津貼修改權限設定</a></li>
 
     <li role="presentation"  style="float: right"><a href="{{url('/api/account_sys/auth/logout')}}">登出</a></li>
     <li role="presentation" style="float: right"><a href="{{url('')}}">回首頁</a></li>
